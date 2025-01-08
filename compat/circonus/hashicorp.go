e30ca647817214904f3fd472775d359b5687b3ec
Matt Keeler <mkeeler@users.noreply.github.com>
2025-01-08 09:49:24 -0500

    Introduce `compat` package to ease migration from the old armon/go-metrice module name to the current hashicorp/go-metrics name (#169)
    * Introduce `compat` package to ease migration from the old armon/go-metrics module name to the current hashicorp/go-metrics name
    
    * Update README.md to include backwards compatibility info
    
    * Fix typos

---
+++ compat/circonus/hashicorp.go
@@ -0,0 +1,15 @@
+//go:build hashicorpmetrics
+// +build hashicorpmetrics
+
+package circonus
+
+import (
+	"github.com/hashicorp/go-metrics/circonus"
+)
+
+type CirconusSink = circonus.CirconusSink
+type Config = circonus.Config
+
+func NewCirconusSink(cc *Config) (*CirconusSink, error) {
+	return circonus.NewCirconusSink(cc)
+}
---
+++ compat/prometheus/armon.go
@@ -0,0 +1,31 @@
+//go:build armonmetrics || ignore || !hashicorpmetrics
+// +build armonmetrics ignore !hashicorpmetrics
+
+package prometheus
+
+import (
+	"time"
+
+	"github.com/armon/go-metrics/prometheus"
+)
+
+var DefaultPrometheusOpts = prometheus.DefaultPrometheusOpts
+
+type CounterDefinition = prometheus.CounterDefinition
+type GaugeDefinition = prometheus.GaugeDefinition
+type PrometheusOpts = prometheus.PrometheusOpts
+type PrometheusPushSink = prometheus.PrometheusPushSink
+type PrometheusSink = prometheus.PrometheusSink
+type SummaryDefinition = prometheus.SummaryDefinition
+
+func NewPrometheusPushSink(address string, pushInterval time.Duration, name string) (*PrometheusPushSink, error) {
+	return prometheus.NewPrometheusPushSink(address, pushInterval, name)
+}
+
+func NewPrometheusSink() (*PrometheusSink, error) {
+	return prometheus.NewPrometheusSink()
+}
+
+func NewPrometheusSinkFrom(opts PrometheusOpts) (*PrometheusSink, error) {
+	return prometheus.NewPrometheusSinkFrom(opts)
+}
